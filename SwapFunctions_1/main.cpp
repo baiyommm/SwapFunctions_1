917915894f3cc7e37ad74c041c897eb28088ed32
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-09-04 11:09:03 -0700

    Swapped the values in the variables using the temp integer. Also, printed out the results of the swapping. Program Successful.

--- SwapFunctions_1/main.cpp
+++ SwapFunctions_1/main.cpp
@@ -9,5 +9,12 @@ int main() {
     
     cout << "The values needed to be swapped are " << number1 << " and " << number2 << endl;
     
+    temp = number1; // Assigning int temp with value in number1
+    number1 = number2; // swap function using initialization
+    number2 = temp;
+    
+    cout << "Now, the numbers in the integers are " << number1 << " and " << number2 << endl;
+    cout << "The numbers in the integers have successfully swapped. End of Program" << endl;
+    
     return 0;
 }
