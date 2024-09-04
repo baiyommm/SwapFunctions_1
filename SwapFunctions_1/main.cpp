f3732f966a6ec81248f2606a85004abc3eb39a4a
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-09-04 11:07:21 -0700

    Created the integers and values needed to be swapped

--- SwapFunctions_1/main.cpp
+++ SwapFunctions_1/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+using namespace std;
 
 int main() {
 
+    int number1 = 2; // Numbers needed to swap
+    int number2 = 1;
+    int temp; // temporary integer to hold the value
+    
+    cout << "The values needed to be swapped are " << number1 << " and " << number2 << endl;
     
     return 0;
 }
